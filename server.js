b2bc7f882a3fbcba4b987b3c91fd6aae56e5ae6f
ashleydb <abennett@gmail.com>
2016-12-02 15:36:29 -0800

    Fixing for localhost
    Fixing https redirect so it works on localhost.

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var app = express();
 // Get a port from an environment variable, (e.g. on Heroku) or fallback to 3000 (e.g. locally)
 const PORT = process.env.PORT || 3000;
 
-// Redirect HTTPS traffic to HTTP, (for use with openWeatherMap API on free Heroku?)
+// Redirect HTTPS traffic to HTTP, (for use with openWeatherMap free API)
 app.use(function(request, response, next) {
-    if (request.headers['x-forwarded-proto'] === 'http') {
-        next();
-    } else {
+    if (request.headers['x-forwarded-proto'] === 'https') {
         response.redirect('http://' + request.hostname + request.url);
+    } else {
+        next();
     }
 });
 
