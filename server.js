8517d7a6f4bb0144e4469e4ee375d84d98a200bf
ashleydb <abennett@gmail.com>
2016-12-02 15:27:30 -0800

    Configuring for Heroku
    Modifying scripts for Heroku deployment

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 // Create express web server
 var express = require('express');
+var app = express();
 
-// Create our app
-var app = express()
+// Get a port from an environment variable, (e.g. on Heroku) or fallback to 3000 (e.g. locally)
+const PORT = process.env.PORT || 3000;
+
+// Redirect HTTPS traffic to HTTP, (for use with openWeatherMap API on free Heroku?)
+app.use(function(request, response, next) {
+    if (request.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        response.redirect('http://' + request.hostname + request.url);
+    }
+});
+
 app.use(express.static('public'));
-app.listen(3000, function() {
-  console.log('Express server is up on port 3000');
-})
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
+});
