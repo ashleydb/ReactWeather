1d2186fde4de9c8d6327d31ebc8debe039858d2b
ashleydb <abennett@gmail.com>
2016-12-02 16:03:31 -0800

    Updating copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,7 +12,10 @@ var React = require('react');
 // rewritten using arror functions from ES6.
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the about page.</p>
+    </div>
   );
 }
 
