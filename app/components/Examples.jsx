a827d2b6f75b015a5201902fd9cfa04a51341e06
ashleydb <abennett@gmail.com>
2016-12-05 16:24:18 -0800

    Styling pages and adding examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,20 +1,17 @@
 var React = require('react');
-
-// var Examples = React.createClass({
-//   render: function() {
-//     return (
-//       <h3>Examples Component</h3>
-//     );
-//   }
-// })
+var {Link} = require('react-router');
 
 //Basic presentational component that would only have a render function
 // rewritten using arror functions from ES6.
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page.</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li><Link to='/?location=London,UK'>London, UK</Link></li>
+        <li><Link to='/?location=San%20Francisco'>San Francisco</Link></li>
+      </ol>
     </div>
   );
 }
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,26 +1,17 @@
 var React = require('react');
 var Nav = require('Nav');
 
-// var Main = React.createClass({
-//   render: function() {
-//     return (
-//       <div>
-//         <Nav/>
-//         <h2>Main Component</h2>
-//         {this.props.children}
-//       </div>
-//     );
-//   }
-// })
-
 //Basic presentational component that would only have a render function
 // rewritten using arror functions from ES6.
 var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h2>Main Component</h2>
-      {props.children}
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 }
